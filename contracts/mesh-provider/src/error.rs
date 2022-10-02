1960a5f19a65e214cf5f96be2082800f00d340ca
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-10-02 02:31:00 +0200

    Enforce channel handshake in both sides

--- contracts/mesh-provider/src/error.rs
+++ contracts/mesh-provider/src/error.rs
@@ -14,6 +14,15 @@ pub enum ContractError {
     #[error("Unauthorized")]
     Unauthorized {},
 
+    #[error("Contract already has a bound channel: {0}")]
+    ChannelExists(String),
+
+    #[error("Unauthorized counterparty chain, awaiting connection '{0}'")]
+    WrongConnection(String),
+
+    #[error("Refuse to respond on unregistered channel '{0}'")]
+    UnknownChannel(String),
+
     #[error("Custom Error val: {val:?}")]
     CustomError { val: String },
     // Add any other custom errors you like here.
