2f8318b6a2351283ba35fc84ab804a5ba3968b2a
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-10-02 02:46:06 +0200

    Expose ibc message types

--- packages/mesh-ibc/src/lib.rs
+++ packages/mesh-ibc/src/lib.rs
@@ -6,6 +6,10 @@ use cosmwasm_std::IbcOrder;
 
 pub use crate::ack::StdAck;
 pub use crate::checks::{check_order, check_version, MeshSecurityError};
+pub use crate::ibc_msg::{
+    ConsumerMsg, ListValidatorsResponse, ProviderMsg, RewardsResponse, StakeResponse,
+    UnstakeResponse, UpdateValidatorsResponse, ValidatorAmount,
+};
 
 pub const IBC_APP_VERSION: &str = "mesh-security-v0.1";
 pub const APP_ORDER: IbcOrder = IbcOrder::Unordered;
