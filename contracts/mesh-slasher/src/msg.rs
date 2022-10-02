c1eced8881309b8e6f69d627a08efb37b87f66d0
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-10-02 04:24:09 +0200

    Define basic interface for slasher

--- contracts/mesh-slasher/src/msg.rs
+++ contracts/mesh-slasher/src/msg.rs
@@ -1,11 +1,26 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Decimal;
 
 #[cw_serde]
 pub struct InstantiateMsg {}
 
+/// This is a mock contract
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    /// Owner can slash validator by X%
+    SubmitEvidence {
+        validator: String,
+        amount: Decimal,
+    }
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    Owner{}
+}
+
+#[cw_serde]
+pub struct OwnerResponse {
+    pub owner: String,
+}
