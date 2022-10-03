bf3e90da0a89c7139261acb2249a59dd194f9e62
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-10-03 09:35:56 +0200

    Write deploy script for provider

--- tests/src/scripts/deploy-contracts.ts
+++ tests/src/scripts/deploy-contracts.ts
@@ -1,1 +1,134 @@
-console.log("foobar");
+import { assert, SigningCosmWasmClient, toBinary } from "cosmwasm";
+
+import { InstantiateMsg as LockupInitMsg } from "../bindings/MeshLockup.types";
+import { MeshProviderClient } from "../bindings/MeshProvider.client";
+import { InstantiateMsg as ProviderInitMsg } from "../bindings/MeshProvider.types";
+
+import { connect, getMnemonic, pprint, setupContracts } from "./helpers";
+import { connections, junoTestConfig, osmoTestConfig } from "./networks";
+
+interface ProviderInfo {
+  meshLockupAddr: string;
+  meshProviderAddr: string;
+  meshProviderPort: string;
+  meshSlasherAddr: string;
+}
+interface ConsumerInfo {
+  foo: string;
+}
+
+async function installProvider(
+  client: SigningCosmWasmClient,
+  signer: string,
+  {
+    connectionId,
+    denom,
+  }: {
+    connectionId: string;
+    denom: string;
+  }
+): Promise<ProviderInfo> {
+  console.debug("Upload contracts to provider...");
+  const providerContracts = {
+    mesh_lockup: "./internal/mesh_lockup.wasm",
+    mesh_provider: "./internal/mesh_provider.wasm",
+    mesh_slasher: "./internal/mesh_slasher.wasm",
+  };
+  const wasmIds = await setupContracts(client, signer, providerContracts);
+
+  const initMeshLockup: LockupInitMsg = { denom };
+  const { contractAddress: meshLockupAddr } = await client.instantiate(
+    signer,
+    wasmIds.mesh_lockup,
+    initMeshLockup,
+    "mesh_lockup contract",
+    "auto"
+  );
+
+  const initMeshProvider: ProviderInitMsg = {
+    consumer: {
+      connection_id: connectionId,
+    },
+    slasher: {
+      code_id: wasmIds.mesh_slasher,
+      msg: toBinary({
+        owner: signer,
+      }),
+    },
+    lockup: meshLockupAddr,
+    // TODO: get real number somehow... look at tendermint client queries or staking?
+    unbonding_period: 86400 * 14,
+  };
+  const { contractAddress: meshProviderAddr } = await client.instantiate(
+    signer,
+    wasmIds.mesh_provider,
+    initMeshProvider,
+    "mesh_provider contract",
+    "auto"
+  );
+  const { ibcPortId: meshProviderPort } = await client.getContract(meshProviderAddr);
+  assert(meshProviderPort);
+
+  // query the newly created slasher
+  const providerClient = new MeshProviderClient(client, signer, meshProviderAddr);
+  const { slasher } = await providerClient.config();
+  const meshSlasherAddr = assert(slasher);
+
+  return { meshLockupAddr, meshProviderAddr, meshProviderPort, meshSlasherAddr };
+}
+
+async function installConsumer(
+  client: SigningCosmWasmClient,
+  address: string,
+  connectionId: string,
+  info: ProviderInfo
+): Promise<ConsumerInfo> {
+  console.debug("Upload contracts to consumer...");
+  const consumerContracts = {
+    mesh_consumer: "./internal/mesh_consumer.wasm",
+    meta_staking: "./internal/meta_staking.wasm",
+  };
+  const wasmIds = await setupContracts(client, address, consumerContracts);
+
+  return { foo: "bar" };
+}
+
+// // sees if it is time to call
+// async function checkTrigger(client: SigningCosmWasmClient) {
+//     const { config } = await client.queryContractSmart(distroAddr, {config:{}});
+//     pprint(config);
+//     const elapsed = Date.now() / 1000 - config.last_payment;
+//     if (elapsed < config.epoch) {
+//         console.log(`Next epoch comes in ${config.epoch - elapsed} seconds`);
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+
+async function main() {
+  const mnemonic = getMnemonic();
+  const provider = await connect(mnemonic, osmoTestConfig);
+  const consumer = await connect(mnemonic, junoTestConfig);
+
+  const connectProvToCons = connections[osmoTestConfig.chainId][junoTestConfig.chainId];
+  const connectConsToProv = connections[junoTestConfig.chainId][osmoTestConfig.chainId];
+  if (connectProvToCons === undefined || connectConsToProv === undefined) {
+    throw Error("Connection not found");
+  }
+
+  const provInfo = await installProvider(provider.client, provider.address, { connectionId: connectProvToCons, denom: osmoTestConfig.feeToken});
+  pprint(provInfo);
+  const consInfo = await installConsumer(consumer.client, consumer.address, connectConsToProv, provInfo);
+  pprint(consInfo);
+}
+
+main().then(
+  () => {
+    process.exit(0);
+  },
+  (error) => {
+    console.error(error);
+    process.exit(1);
+  }
+);
--- tests/src/scripts/helpers.ts
+++ tests/src/scripts/helpers.ts
@@ -1,74 +1,62 @@
+import { readFileSync } from "fs";
 import { env } from "process";
 
 import { DirectSecp256k1HdWallet, makeCosmoshubPath, SigningCosmWasmClient } from "cosmwasm";
 
-import { junoTestConfig, Network, osmoTestConfig } from "./networks";
+import { Network } from "./networks";
 
-const pprint = (x: unknown) => console.log(JSON.stringify(x, undefined, 2));
+export const pprint = (x: unknown) => console.log(JSON.stringify(x, undefined, 2));
 
 // Check "MNEMONIC" env variable and ensure it is set to a reasonable value
-function getMnemonic(): string {
-    const mnemonic = env["MNEMONIC"];
-    if (!mnemonic || mnemonic.length < 48) {
-        throw new Error("Must set MNEMONIC to a 12 word phrase");
-    }
-    return mnemonic;
+export function getMnemonic(): string {
+  const mnemonic = env["MNEMONIC"];
+  if (!mnemonic || mnemonic.length < 48) {
+    throw new Error("Must set MNEMONIC to a 12 word phrase");
+  }
+  return mnemonic;
 }
 
-async function connect(mnemonic: string, network: Network) {
-    const { prefix, gasPrice, feeToken, rpcEndpoint } = network;
-    const hdPath = makeCosmoshubPath(0);
+export async function connect(mnemonic: string, network: Network) {
+  const { prefix, gasPrice, feeToken, rpcEndpoint } = network;
+  const hdPath = makeCosmoshubPath(0);
 
-    // Setup signer
-    const offlineSigner = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix, hdPaths: [hdPath] });
-    const { address } = (await offlineSigner.getAccounts())[0];
-    console.log(`Connected to ${address}`);
-    
-    // Init SigningCosmWasmClient client
-    const client = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, offlineSigner, {
-        prefix,
-        gasPrice,
-    });
-    const balance = await client.getBalance(address, feeToken);
-    console.log(`Balance: ${balance.amount} ${balance.denom}`);
-  
-    const chainId = await client.getChainId();
-  
-    if (chainId !== network.chainId) {
-        throw Error("Given ChainId doesn't match the clients ChainID!");
-    }
-  
-    return { client, address };
+  // Setup signer
+  const offlineSigner = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix, hdPaths: [hdPath] });
+  const { address } = (await offlineSigner.getAccounts())[0];
+  console.log(`Connected to ${address}`);
+
+  // Init SigningCosmWasmClient client
+  const client = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, offlineSigner, {
+    prefix,
+    gasPrice,
+  });
+  const balance = await client.getBalance(address, feeToken);
+  console.log(`Balance: ${balance.amount} ${balance.denom}`);
+
+  const chainId = await client.getChainId();
+
+  if (chainId !== network.chainId) {
+    throw Error("Given ChainId doesn't match the clients ChainID!");
+  }
+
+  return { client, address };
 }
 
-// // sees if it is time to call
-// async function checkTrigger(client: SigningCosmWasmClient) {
-//     const { config } = await client.queryContractSmart(distroAddr, {config:{}});
-//     pprint(config);
-//     const elapsed = Date.now() / 1000 - config.last_payment;
-//     if (elapsed < config.epoch) {
-//         console.log(`Next epoch comes in ${config.epoch - elapsed} seconds`);
-//         return false;
-//     } else {
-//         return true;
-//     }
-// }
+export async function setupContracts(
+  client: SigningCosmWasmClient,
+  signer: string,
+  contracts: Record<string, string>
+): Promise<Record<string, number>> {
+  const results: Record<string, number> = {};
 
-async function main() {
-    const mnemonic = getMnemonic();
-    const provider = await connect(mnemonic, osmoTestConfig);
-    const consumer = await connect(mnemonic, junoTestConfig);
-    // if (await checkTrigger(client)) {
-    //     await pingTrigger(client, address);
-    // }
+  for (const name in contracts) {
+    const path = contracts[name];
+    console.info(`Storing ${name} from ${path}...`);
+    const wasm = await readFileSync(path);
+    const receipt = await client.upload(signer, wasm, "auto", `Upload ${name}`);
+    console.debug(`Upload ${name} with CodeID: ${receipt.codeId}`);
+    results[name] = receipt.codeId;
+  }
+
+  return results;
 }
-
-main().then(
-    () => {
-        process.exit(0);
-    },
-    (error) => {
-        console.error(error);
-        process.exit(1);
-    },
-);
