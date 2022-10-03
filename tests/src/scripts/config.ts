6d40e30d7202476da5b79dd972591c44665fc516
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-10-03 22:17:29 +0200

    Successful deploy

--- tests/src/scripts/config.ts
+++ tests/src/scripts/config.ts
@@ -1,3 +1,12 @@
-export const config = {
-  distroAddr: "juno1yu7s35r3e43j635c7wxfev9876tvfpjf6n5803dwu6e3z8qrmvzshwk075",
+export const osmoConfig = {
+  meshLockupAddr: "osmo1kf88a6ftgt57t2hg48jfc9679l50tq6jqwsdheuld6467jls2hxstnvnal",
+  meshProviderAddr: "osmo19kx57u0nrhqas34qvyhu2ydt4a9rlj6n37l88jqwfc2eesuk9tuq6c6ywj",
+  meshProviderPort: "wasm.osmo19kx57u0nrhqas34qvyhu2ydt4a9rlj6n37l88jqwfc2eesuk9tuq6c6ywj",
+  meshSlasherAddr: "osmo1fewayl23e89rkejx932ljxf0ylsmtqc49r4myzzsvxcpweptmu0szshpkv"
 };
+
+export const junoConfig = {
+  metaStakingAddr: "juno15tuw2fdyl666j7569ynt9dpdqykwzqw4d4sxrf0smcezudeafedsqu7n97",
+  meshConsumerAddr: "juno12e2p5rgcmcgu2t63mwystxzlfwf50kpgn9gjlnz2sf6q76pnkcjqrna22u",
+  meshConsumerPort: "wasm.juno12e2p5rgcmcgu2t63mwystxzlfwf50kpgn9gjlnz2sf6q76pnkcjqrna22u"
+}
