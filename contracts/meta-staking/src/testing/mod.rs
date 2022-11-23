faeb2a7eaee09de69f13b783b9b20404c2358950
Art3mix <r3mix.il@gmail.com>
2022-11-23 14:07:05 +0200

    add assert_error macro

--- contracts/meta-staking/src/testing/mod.rs
+++ contracts/meta-staking/src/testing/mod.rs
@@ -7,3 +7,14 @@ const NATIVE_DENOM: &str = "NATIVE_DENOM";
 const CONSUMER_1: &str = "consumer_1";
 const CONSUMER_2: &str = "consumer_2";
 const VALIDATOR: &str = "validator";
+
+/// Assert the error is the error we
+///
+/// Result<AppResponse, Error> === ContractError::Example{}
+macro_rules! assert_error {
+    ($x:expr, $e:expr) => {
+        assert_eq!($x.unwrap_err().root_cause().to_string(), $e.to_string())
+    };
+}
+
+pub(crate) use assert_error;
