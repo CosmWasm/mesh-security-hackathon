f74441da9a508a3a5fd9a3c2b862ab533c43197d
Art3mix <r3mix.il@gmail.com>
2022-11-23 10:43:03 +0200

    small helper fixes

--- contracts/meta-staking/src/testing/utils/executes.rs
+++ contracts/meta-staking/src/testing/utils/executes.rs
@@ -68,7 +68,7 @@ pub fn withdraw_rewards(
 
 pub fn add_consumer(
     app: &mut App,
-    contract_addr: Addr,
+    contract_addr: &str,
     sender: &str,
     consumer_addr: &str,
     funds_avaiable: u128,
@@ -80,7 +80,7 @@ pub fn add_consumer(
 
     app.execute_contract(
         addr!(sender),
-        contract_addr.clone(),
+        addr!(contract_addr),
         &ExecuteMsg::Sudo(sudo_msg),
         &[],
     )
@@ -88,7 +88,7 @@ pub fn add_consumer(
 
 pub fn remove_consumer(
     app: &mut App,
-    contract_addr: Addr,
+    contract_addr: &str,
     sender: &str,
     consumer_addr: &str,
 ) -> AnyResult<AppResponse> {
@@ -98,7 +98,7 @@ pub fn remove_consumer(
 
     app.execute_contract(
         addr!(sender),
-        contract_addr.clone(),
+        addr!(contract_addr),
         &ExecuteMsg::Sudo(sudo_msg),
         &[],
     )
