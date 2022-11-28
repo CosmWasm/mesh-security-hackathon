4cd6c042a364eb9a59c087b2879b12dfba97b5cd
Art3mix <r3mix.il@gmail.com>
2022-11-28 19:15:10 +0200

    added failing tests

--- contracts/mesh-consumer/src/testing/mod.rs
+++ contracts/mesh-consumer/src/testing/mod.rs
@@ -1,5 +1,6 @@
 pub mod utils;
 
 pub mod test_contract;
-pub mod test_ibc_receive;
+pub mod test_ibc;
 pub mod test_ibc_ack;
+pub mod test_ibc_receive;
--- contracts/mesh-consumer/src/testing/test_ibc_ack.rs
+++ contracts/mesh-consumer/src/testing/test_ibc_ack.rs
@@ -5,13 +5,18 @@ use mesh_testing::constants::{NATIVE_DENOM, VALIDATOR};
 use crate::{
     ibc::build_timeout,
     testing::utils::helpers::{ICS20_CHANNEL_ID, REMOTE_PORT},
+    ContractError,
 };
 
-use super::utils::{executes::ibc_ack_rewards, helpers::to_ack_success, setup::setup_with_channel};
+use super::utils::{
+    executes::ibc_ack_rewards,
+    helpers::{to_ack_error, to_ack_success},
+    setup::setup_with_channel,
+};
 
 #[test]
 fn test_ibc_ack_rewards() {
-    let (mut deps, _) = setup_with_channel();
+    let (mut deps, _) = setup_with_channel(None);
 
     // We test a successful ack
     let ack = IbcAcknowledgement::new(to_ack_success(RewardsResponse {}));
@@ -35,7 +40,21 @@ fn test_ibc_ack_rewards() {
     );
 }
 
+// TODO: update_validators doesn't do anything, should we just delete it?
 #[test]
+#[ignore]
 fn test_ibc_update_validators() {
     unimplemented!()
+}
+
+// TODO: on failed ack we should revert stuff based on sent msg,
+// leaving todo here to make the tests better later.
+#[test]
+fn test_ibc_ack_failed() {
+    let (mut deps, _) = setup_with_channel(None);
+
+    // We test a error ack
+    let ack = IbcAcknowledgement::new(to_ack_error("Something went wrong"));
+    let err = ibc_ack_rewards(deps.as_mut(), VALIDATOR, 100, ack).unwrap_err();
+    assert_eq!(err, ContractError::AckFailed {})
 }
